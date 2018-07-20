639ad467ba0ad9fcbf10e1b67bea8515fc44def9
风吹雪 <yunruofengsheng@126.com>
2018-07-20 19:22:20 +0800

    'chagne'

---
+++ client/app.js
@@ -0,0 +1,10 @@
+//app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
+
+App({
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+    }
+})
---
+++ server/controllers/demo.js
@@ -0,0 +1,6 @@
+// 默认导出当前文件夹下的映射
+module.exports = ctx = () => {
+  ctx.state.data = {
+    message: 'Hello World'
+  }
+}
